75f415a53c501bf21aa636810845d09028ed84d9
SoraY677 <SoraY677@outlook.com>
2020-06-23 23:55:19 +0900

    プレイヤーのショットを追加

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pyxel
+from shotBallet import ShotBallet
 
 
 class Player:
@@ -11,6 +12,7 @@ class Player:
         self.size = 16
         self.x = (panelw - self.size) / 2
         self.y = panelh - self.size - 5
+        self.shotBallet = ShotBallet(-5, 5, 20)
 
     def update(self, panelw, panelh):
         '''
@@ -21,6 +23,8 @@ class Player:
             self.x += 3
         elif pyxel.btn(pyxel.KEY_LEFT):
             self.x -= 3
+        if pyxel.btn(pyxel.KEY_SPACE):
+            self.shotBallet.init(self.x + self.size/2, self.y)
 
         # 壁で止まる
         if self.x < 0:
@@ -28,8 +32,11 @@ class Player:
         elif self.x > panelw-self.size:
             self.x = panelw-self.size
 
+        self.shotBallet.update(panelh)
+
     def draw(self):
         '''
         プレイヤーの描画を行う
         '''
         pyxel.blt(self.x, self.y, 1, 16*3, 0, 16, 16, 0)
+        self.shotBallet.draw()
